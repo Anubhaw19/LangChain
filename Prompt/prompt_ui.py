ad4eb406dfede94ef3754ddc3cf5d3d4ad4b348c
anubhaw19 <anubhawapp@gmail.com>
2025-05-25 22:32:36 +0530

    prompt chain

--- Prompt/prompt_ui.py
+++ Prompt/prompt_ui.py
@@ -2,7 +2,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -16,21 +16,23 @@ style_input  = st.selectbox("Select Explanation Style",[ "Beginner-Friendly","Te
 length_input = st.selectbox("Select Explanation Length",["Short (1-2 paragraph)","Medium (3-5 paragraph)", "Long (detailed explanation)"])
 
 #prompt tempalte
-template = PromptTemplate(
-    template = """
-Please summarise the research paper titled "{paper_input}" with the following specifications:
-Explanation  Style : {style_input}
-Explanation Length : {length_input}
-1.Mathematical Details : 
- - Include relevant mathematical equations if present in the paper.
- - Explain the mathematical concepts using simple, ituitive code snippets where applicable.
-2.Analogies:
- - Use relatable analogies to simplify complex ideas.
-If certain information is not available instead of guessing. Ensure the summary is clear, accurate, and aligned with the provided style and length, also don't show the unnecessary text , just the content
-""",
-input_varibles = ['paper_input','style_input','length_input'],
-validate_template = False
-)
+# template = PromptTemplate(
+#     template = """
+# Please summarise the research paper titled "{paper_input}" with the following specifications:
+# Explanation  Style : {style_input}
+# Explanation Length : {length_input}
+# 1.Mathematical Details : 
+#  - Include relevant mathematical equations if present in the paper.
+#  - Explain the mathematical concepts using simple, ituitive code snippets where applicable.
+# 2.Analogies:
+#  - Use relatable analogies to simplify complex ideas.
+# If certain information is not available instead of guessing. Ensure the summary is clear, accurate, and aligned with the provided style and length, also don't show the unnecessary text , just the content
+# """,
+# input_varibles = ['paper_input','style_input','length_input'],
+# validate_template = False
+# )
+
+template = load_prompt('template.json')
 
 #fill the place holders
 
