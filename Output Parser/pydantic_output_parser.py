7c307cfaebd4a0889907057fc484d5b888c6fe7d
anubhaw19 <anubhawapp@gmail.com>
2025-06-15 00:08:40 +0530

    Pydantic Output Parser

---
+++ Output Parser/pydantic_output_parser.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = 'gemini-2.0-flash')
+
+class Mobile(BaseModel):
+    brand: str = Field(description = "name of the mobile phone brand")
+    model: str = Field(description = 'name of the mobile model')
+    price : int = Field(description = "launch price of the mobile model in INR")
+    os: str = Field(description = "Oerating system of the mobile model")
+    launch_date : str = Field(description = "Lauch date of the model in India")
+
+parser = PydanticOutputParser(pydantic_object = Mobile)
+
+template = PromptTemplate(
+    template = 'Give the specification of {model_name} mobile model \n {format_instruction}',
+    input_variables = ['model_name'],
+    partial_variables = {'format_instruction' : parser.get_format_instructions()}
+)
+
+prompt = template.invoke({'model_name':'Googgle Pixel 6'})
+
+# print(prompt)
+# result = model.invoke(prompt)
+# final_result = parser.parse(result.content)
+# print(final_result)
+
+#invoke model using chain
+chain = template | model | parser
+result = chain.invoke({'model_name':'Oneplus Nord 4'})
+print(result)
